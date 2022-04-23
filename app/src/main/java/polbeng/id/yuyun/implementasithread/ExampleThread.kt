17e867a13a36e82775ccabd69b0bbab9e0b99a24
RPL-YuniZanika <yuyunisyerin@gmail.com>
2022-04-23 10:32:00 +0700

    Implementasi Thread

---
+++ app/src/main/java/polbeng/id/yuyun/implementasithread/ExampleThread.kt
@@ -0,0 +1,23 @@
+package polbeng.id.yuyun.implementasithread
+
+import android.util.Log
+
+class ExampleThread : Thread {
+    var seconds = 0
+    val TAG = "MainActivity"
+
+    constructor(seconds: Int) : super() {
+        this.seconds = seconds
+    }
+
+    override fun run() {
+        for (i in 1 until seconds){
+            Log.d(TAG, "startThread: $i")
+            try {
+                Thread.sleep(1000)
+            } catch (ie: InterruptedException){
+                ie.printStackTrace()
+            }
+        }
+    }
+}
